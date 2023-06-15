0d762a511b1a3c884b1041195861d8df7a527169
r-land <labofsamplea@gmail.com>
2023-06-15 22:36:13 +0900

    :sparkles: update item entity for mapping

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -5,19 +5,38 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @ToString
 @Getter
 @Setter
+@Entity
+@Table(name="item")
 public class Item {
 
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; //상품코드
+
+    @Column(nullable = false, length = 50)
     private String itemNm; //상품명
+
+    @Column(name="price", nullable = false)
     private int price; //가격
-    private int stockNumber;
+
+    @Column(nullable = false)
+    private int stockNumber; //재고수량
+
+    @Lob
+    @Column(nullable = false)
     private String itemDetail; //상품상세설명
+
+    @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSelLStatus; //상품판매상태
+
     private LocalDateTime regTime; //등록시간
+
     private LocalDateTime updateTime; //수정시간
 }
